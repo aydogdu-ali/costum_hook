6b6613dc36e1daba3a41c7e14b5f18fe04009768
Ali <ali.ozgur@example.com>
2023-01-30 01:58:50 +0300

    edit components css

--- src/components/picture.jsx
+++ src/components/picture.jsx
@@ -1,9 +1,18 @@
 import React from 'react'
+import { useState } from 'react'
 
-const picture = () => {
+
+
+const Picture = () => {
+ const url = `https://pixabay.com/api/videos/?key=31725179-e9547203f59a4095ebc0c6c08&q=${text}&video_type=film&lang=tr&per_page=4`;
+    const [text, setText] = useState("")
   return (
-    <div>picture</div>
+    <div className="picture">
+        <h1>Fotoğraflar</h1>
+        <input type="search"  placeholder='search' value = {text}onChange={(e)=> setText(e.target.value)}/>
+        <div className="show"></div>
+        </div>
   )
 }
 
-export default picture
+export default Picture
--- src/components/video.jsx
+++ src/components/video.jsx
@@ -1,9 +1,23 @@
 import React from 'react'
+import { useState } from 'react';
 
-const video = () => {
+const Video = () => {
+    const url = `https://pixabay.com/api/?key=31725179-e9547203f59a4095ebc0c6c08&q=${text}&image_type=photo&lang=tr&per_page=4`;
+      const [text, setText] = useState("");
   return (
-    <div>video</div>
-  )
+    <div className="video">
+      <h1>Videolar </h1>
+      <form action="">
+        <input
+          type="search"
+          placeholder="search"
+          value={text}
+          onChange={(e) => setText(e.target.value)}
+        />
+        <div className="göster"></div>
+      </form>
+    </div>
+  );
 }
 
-export default video
+export default Video
